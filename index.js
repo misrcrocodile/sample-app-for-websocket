7df90e29f49577e340bba535cb305e6308569af3
AnhNT <anhnt14@rikkeisoft.com>
2023-01-03 15:16:46 +0900

    change to old style

--- index.js
+++ index.js
@@ -1,22 +1,109 @@
 #!/usr/bin/env node
-const PORT = 3000;
-const INDEX = '/index.html';
 
-const server = express()
-  .use((req, res) => res.sendFile(INDEX, { root: __dirname }))
-  .listen(PORT, () => console.log(`Listening on ${PORT}`));
+var WebSocketServer = require('websocket').server;
+var http = require('http');
+var port = process.env.PORT || 8011;
+var server = http.createServer(function (request, response) {
+    console.log((new Date()) + ' Received request for ' + request.url);
+    response.writeHead(404);
+    response.end();
+});
+server.listen(port, function () {
+    console.log((new Date()) + ' Server is listening on port ' + port);
+});
+wsServer = new WebSocketServer({
+    httpServer: server,
+    // You should not use autoAcceptConnections for production 
+    // applications, as it defeats all standard cross-origin protection 
+    // facilities built into the protocol and the browser.  You should 
+    // *always* verify the connection's origin and decide whether or not 
+    // to accept it. 
+    // autoAcceptConnections: false
+});
 
-  const { Server } = require('ws');
+const TYPE_ESP = "esp8266";
+const TYPE_WEB = "webclient";
+const TYPE_PI = "piclient";
 
-const wss = new Server({ server });
+function originIsAllowed(req) {
+    // put logic here to detect whether the specified origin is allowed. 
+    var approveList = [TYPE_ESP, TYPE_WEB, TYPE_PI];
+    var protocol = req.requestedProtocols[0];
+    return approveList.includes(protocol);
+}
+var espConn = null,
+    webConn = null,
+    piConn = null;
 
-wss.on('connection', (ws) => {
-    console.log('Client connected');
-    ws.on('close', () => console.log('Client disconnected'));
-  });
+wsServer.on('request', function (request) {
+    var requestType = request.requestedProtocols[0];
+    if (!originIsAllowed(request)) {
+        request.reject();
+        console.log((new Date()) + ' Connection from origin ' + request.origin + ' rejected. requestProtocol' + request.requestedProtocols[0]);
+        return;
+    }
+    var connection = request.accept(requestType, request.origin);
+    handleConnection(connection, requestType); // assign connection to list 3 kind of connection espConn, webConn, piConn via connect type
+    console.log((new Date()) + requestType + ' Connection accepted.');
 
-  setInterval(() => {
-    wss.clients.forEach((client) => {
-      client.send(new Date().toTimeString());
+    connection.on('message', function (message) {
+        handleMessage(connection, message, requestType);
     });
-  }, 1000);
+
+    connection.on('close', function (reasonCode, description) {
+        handleClose(connection, requestType);
+
+    });
+});
+
+function isHex(pStr) {
+    return /^[0-9A-Fa-f]{7}$/.test(pStr);
+}
+
+function handleConnection(conn, type) {
+    switch (type) {
+        case TYPE_ESP:
+            espConn = conn;
+            break;
+        case TYPE_WEB:
+            webConn = conn;
+            break;
+        case TYPE_PI:
+            piConn = conn;
+            break;
+    }
+}
+
+function handleMessage(conn, mess, type) {
+
+    switch (type) {
+        case TYPE_ESP:
+            break;
+        case TYPE_WEB:
+            if (espConn) {
+                espConn.sendUTF(mess.utf8Data);
+            }
+            break;
+        case TYPE_PI:
+            if (espConn) {
+                espConn.sendUTF(mess.utf8Data);
+            }
+            break;
+    }
+    console.log('Received Message: ' + mess.utf8Data);
+}
+
+function handleClose(conn, type) {
+    switch (type) {
+        case TYPE_ESP:
+            espConn = null;
+            break;
+        case TYPE_WEB:
+            webConn = null;
+            break;
+        case TYPE_PI:
+            piConn = null;
+            break;
+    }
+    console.log((new Date()) + ' Peer ' + conn.remoteAddress + ' disconnected.');
+}
